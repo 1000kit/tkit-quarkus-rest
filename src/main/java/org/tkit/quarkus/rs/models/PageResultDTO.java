388621e3c895df754eb207f8d740c35d20649dc7
Andrej Petras <andrej.petras@capgemini.com>
2020-04-03 17:43:25 +0200

    Refactoring models, add the documentation, add REST exception and REST exception mapper.

---
+++ src/main/java/org/tkit/quarkus/rs/models/PageResultDTO.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 tkit.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tkit.quarkus.rs.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PageResultDTO<T> {
+
+    /**
+     * The total elements in the database.
+     */
+    private long totalElements;
+
+    /**
+     * The page index.
+     */
+    private int number;
+
+    /**
+     * The page size.
+     */
+    private int size;
+
+    /**
+     * The number of pages.
+     */
+    private long totalPages;
+
+    /**
+     * The data stream.
+     */
+    private List<T> stream;
+
+}
