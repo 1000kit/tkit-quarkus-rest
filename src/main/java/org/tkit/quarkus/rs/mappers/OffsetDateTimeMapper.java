2132c9fdc96da1cc447a7495c7a617b4ec471ba7
Andrej Petras <andrej.petras@capgemini.com>
2020-10-29 15:22:47 +0100

    Fix localdatetiem zone offset issue

--- src/main/java/org/tkit/quarkus/rs/mappers/OffsetDateTimeMapper.java
+++ src/main/java/org/tkit/quarkus/rs/mappers/OffsetDateTimeMapper.java
@@ -1,35 +1,27 @@
 package org.tkit.quarkus.rs.mappers;
 
-import org.eclipse.microprofile.config.ConfigProvider;
 import org.mapstruct.Mapper;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 
 @Mapper
 public abstract class OffsetDateTimeMapper {
 
-    static final ZoneOffset ZONE_OFFSET = ZoneId.of(
-            ConfigProvider.getConfig()
-                    .getOptionalValue("quarkus.hibernate-orm.jdbc.timezone", String.class)
-                    .orElse(ZoneOffset.UTC.getId())
-    ).getRules().getOffset(Instant.now());
-
     public OffsetDateTime map(LocalDateTime dateTime) {
         if (dateTime == null) {
             return null;
         }
-        return OffsetDateTime.of(dateTime,ZONE_OFFSET);
+        return OffsetDateTime.of(dateTime, ZoneOffset.systemDefault().getRules().getOffset(Instant.now()));
     }
 
     public LocalDateTime map(OffsetDateTime offsetDateTime) {
         if (offsetDateTime == null) {
             return null;
         }
-        return LocalDateTime.ofInstant(offsetDateTime.toInstant(), ZONE_OFFSET);
+        return LocalDateTime.ofInstant(offsetDateTime.toInstant(), ZoneOffset.systemDefault());
     }
 
 }
